1cb93f8afad3bbb2ff045784a6a3322c07913914
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:35:26 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "kubernetes_cluster_node_pools" {
+  description = "All kubernetes_cluster_node_pool resources"
+  value       = azurerm_kubernetes_cluster_node_pool.kubernetes_cluster_node_pools
+}
+
---
+++ variables.tf
@@ -0,0 +1,216 @@
+variable "kubernetes_cluster_node_pools" {
+  description = <<EOT
+Map of kubernetes_cluster_node_pools, attributes below
+Required:
+    - kubernetes_cluster_id
+    - name
+Optional:
+    - auto_scaling_enabled
+    - capacity_reservation_group_id
+    - eviction_policy
+    - fips_enabled
+    - gpu_driver
+    - gpu_instance
+    - host_encryption_enabled
+    - host_group_id
+    - kubelet_disk_type
+    - max_count
+    - max_pods
+    - min_count
+    - mode
+    - node_count
+    - node_labels
+    - node_public_ip_enabled
+    - node_public_ip_prefix_id
+    - node_taints
+    - orchestrator_version
+    - os_disk_size_gb
+    - os_disk_type
+    - os_sku
+    - os_type
+    - pod_subnet_id
+    - priority
+    - proximity_placement_group_id
+    - scale_down_mode
+    - snapshot_id
+    - spot_max_price
+    - tags
+    - temporary_name_for_rotation
+    - ultra_ssd_enabled
+    - vm_size
+    - vnet_subnet_id
+    - workload_runtime
+    - zones
+    - kubelet_config (block):
+        - allowed_unsafe_sysctls (optional)
+        - container_log_max_line (optional)
+        - container_log_max_size_mb (optional)
+        - cpu_cfs_quota_enabled (optional)
+        - cpu_cfs_quota_period (optional)
+        - cpu_manager_policy (optional)
+        - image_gc_high_threshold (optional)
+        - image_gc_low_threshold (optional)
+        - pod_max_pid (optional)
+        - topology_manager_policy (optional)
+    - linux_os_config (block):
+        - swap_file_size_mb (optional)
+        - sysctl_config (optional, block):
+            - fs_aio_max_nr (optional)
+            - fs_file_max (optional)
+            - fs_inotify_max_user_watches (optional)
+            - fs_nr_open (optional)
+            - kernel_threads_max (optional)
+            - net_core_netdev_max_backlog (optional)
+            - net_core_optmem_max (optional)
+            - net_core_rmem_default (optional)
+            - net_core_rmem_max (optional)
+            - net_core_somaxconn (optional)
+            - net_core_wmem_default (optional)
+            - net_core_wmem_max (optional)
+            - net_ipv4_ip_local_port_range_max (optional)
+            - net_ipv4_ip_local_port_range_min (optional)
+            - net_ipv4_neigh_default_gc_thresh1 (optional)
+            - net_ipv4_neigh_default_gc_thresh2 (optional)
+            - net_ipv4_neigh_default_gc_thresh3 (optional)
+            - net_ipv4_tcp_fin_timeout (optional)
+            - net_ipv4_tcp_keepalive_intvl (optional)
+            - net_ipv4_tcp_keepalive_probes (optional)
+            - net_ipv4_tcp_keepalive_time (optional)
+            - net_ipv4_tcp_max_syn_backlog (optional)
+            - net_ipv4_tcp_max_tw_buckets (optional)
+            - net_ipv4_tcp_tw_reuse (optional)
+            - net_netfilter_nf_conntrack_buckets (optional)
+            - net_netfilter_nf_conntrack_max (optional)
+            - vm_max_map_count (optional)
+            - vm_swappiness (optional)
+            - vm_vfs_cache_pressure (optional)
+        - transparent_huge_page (optional)
+        - transparent_huge_page_defrag (optional)
+        - transparent_huge_page_enabled (optional)
+    - node_network_profile (block):
+        - allowed_host_ports (optional, block):
+            - port_end (optional)
+            - port_start (optional)
+            - protocol (optional)
+        - application_security_group_ids (optional)
+        - node_public_ip_tags (optional)
+    - upgrade_settings (block):
+        - drain_timeout_in_minutes (optional)
+        - max_surge (optional)
+        - max_unavailable (optional)
+        - node_soak_duration_in_minutes (optional)
+        - undrainable_node_behavior (optional)
+    - windows_profile (block):
+        - outbound_nat_enabled (optional)
+EOT
+
+  type = map(object({
+    kubernetes_cluster_id         = string
+    name                          = string
+    os_disk_size_gb               = optional(number)
+    os_disk_type                  = optional(string, "Managed")
+    os_sku                        = optional(string)
+    os_type                       = optional(string, "Linux")
+    pod_subnet_id                 = optional(string)
+    priority                      = optional(string, "Regular")
+    proximity_placement_group_id  = optional(string)
+    scale_down_mode               = optional(string, "Delete")
+    snapshot_id                   = optional(string)
+    spot_max_price                = optional(number, -1)
+    tags                          = optional(map(string))
+    temporary_name_for_rotation   = optional(string)
+    ultra_ssd_enabled             = optional(bool, false)
+    vm_size                       = optional(string)
+    vnet_subnet_id                = optional(string)
+    orchestrator_version          = optional(string)
+    node_taints                   = optional(list(string))
+    node_public_ip_prefix_id      = optional(string)
+    node_public_ip_enabled        = optional(bool)
+    auto_scaling_enabled          = optional(bool)
+    capacity_reservation_group_id = optional(string)
+    eviction_policy               = optional(string)
+    fips_enabled                  = optional(bool)
+    gpu_driver                    = optional(string)
+    gpu_instance                  = optional(string)
+    host_encryption_enabled       = optional(bool)
+    host_group_id                 = optional(string)
+    kubelet_disk_type             = optional(string)
+    max_count                     = optional(number)
+    max_pods                      = optional(number)
+    min_count                     = optional(number)
+    mode                          = optional(string, "User")
+    node_count                    = optional(number)
+    node_labels                   = optional(map(string))
+    workload_runtime              = optional(string)
+    zones                         = optional(set(string))
+    kubelet_config = optional(object({
+      allowed_unsafe_sysctls    = optional(set(string))
+      container_log_max_line    = optional(number)
+      container_log_max_size_mb = optional(number)
+      cpu_cfs_quota_enabled     = optional(bool, true)
+      cpu_cfs_quota_period      = optional(string)
+      cpu_manager_policy        = optional(string)
+      image_gc_high_threshold   = optional(number)
+      image_gc_low_threshold    = optional(number)
+      pod_max_pid               = optional(number)
+      topology_manager_policy   = optional(string)
+    }))
+    linux_os_config = optional(object({
+      swap_file_size_mb = optional(number)
+      sysctl_config = optional(object({
+        fs_aio_max_nr                      = optional(number)
+        fs_file_max                        = optional(number)
+        fs_inotify_max_user_watches        = optional(number)
+        fs_nr_open                         = optional(number)
+        kernel_threads_max                 = optional(number)
+        net_core_netdev_max_backlog        = optional(number)
+        net_core_optmem_max                = optional(number)
+        net_core_rmem_default              = optional(number)
+        net_core_rmem_max                  = optional(number)
+        net_core_somaxconn                 = optional(number)
+        net_core_wmem_default              = optional(number)
+        net_core_wmem_max                  = optional(number)
+        net_ipv4_ip_local_port_range_max   = optional(number)
+        net_ipv4_ip_local_port_range_min   = optional(number)
+        net_ipv4_neigh_default_gc_thresh1  = optional(number)
+        net_ipv4_neigh_default_gc_thresh2  = optional(number)
+        net_ipv4_neigh_default_gc_thresh3  = optional(number)
+        net_ipv4_tcp_fin_timeout           = optional(number)
+        net_ipv4_tcp_keepalive_intvl       = optional(number)
+        net_ipv4_tcp_keepalive_probes      = optional(number)
+        net_ipv4_tcp_keepalive_time        = optional(number)
+        net_ipv4_tcp_max_syn_backlog       = optional(number)
+        net_ipv4_tcp_max_tw_buckets        = optional(number)
+        net_ipv4_tcp_tw_reuse              = optional(bool)
+        net_netfilter_nf_conntrack_buckets = optional(number)
+        net_netfilter_nf_conntrack_max     = optional(number)
+        vm_max_map_count                   = optional(number)
+        vm_swappiness                      = optional(number)
+        vm_vfs_cache_pressure              = optional(number)
+      }))
+      transparent_huge_page         = optional(string)
+      transparent_huge_page_defrag  = optional(string)
+      transparent_huge_page_enabled = optional(string)
+    }))
+    node_network_profile = optional(object({
+      allowed_host_ports = optional(object({
+        port_end   = optional(number)
+        port_start = optional(number)
+        protocol   = optional(string)
+      }))
+      application_security_group_ids = optional(list(string))
+      node_public_ip_tags            = optional(map(string))
+    }))
+    upgrade_settings = optional(object({
+      drain_timeout_in_minutes      = optional(number)
+      max_surge                     = optional(string)
+      max_unavailable               = optional(string)
+      node_soak_duration_in_minutes = optional(number)
+      undrainable_node_behavior     = optional(string)
+    }))
+    windows_profile = optional(object({
+      outbound_nat_enabled = optional(bool, true)
+    }))
+  }))
+}
+
